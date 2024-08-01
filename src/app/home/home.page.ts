9dd78af6250d5c00ed37abf58d4824909b48146e
pakin sukkho <pakinsukko@gmail.com>
2024-08-01 12:08:12 +0700

    app01 homework

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonGrid, IonCol, IonRow, IonButton, IonIcon} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trash } from 'ionicons/icons';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonGrid, IonCol, IonRow, IonButton, IonIcon],
+})
+export class HomePage {
+  constructor() {
+    addIcons({trash});
+  }
+}
